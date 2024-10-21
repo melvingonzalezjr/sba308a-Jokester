c32c3f4b0250fd120cfc60065850efc31250cfd6
melvingonzalezjr <mgonzalez713@gmail.com>
2024-10-21 10:41:50 -0500

    Edited dom.js to change website UI upon user interaction

--- javascript/dom.js
+++ javascript/dom.js
@@ -1,2 +1,30 @@
-
+// Function to display jokes
+export function displayJokes(jokes) {
+    const jokesContainer = document.getElementById('jokesContainer');
+    jokesContainer.innerHTML = '';  // refreshed output from previous jokes
+    
+    /*
+    For each element of jokes array:
+        - we create a div element called jokeElement
+        - add a joke object to jokeElement div element
+        - innerHTML is the joke.setup aka the beginning question/riddle of a joke
+        - punchline is the actual humorous final part of joke
+    */
+    jokes.forEach(joke => {
+      const jokeElement = document.createElement('div');
+      jokeElement.classList.add('joke');
+      jokeElement.innerHTML = `
+        <p><strong>${joke.setup}</strong></p>
+        <p>${joke.punchline}</p>
+      `;
+      jokesContainer.appendChild(jokeElement); //remember you gotta finish with appending the child to the DOM element you're editing
+    });
+  }
+  
+  // Function to display an error message
+  export function displayError() {
+    const jokesContainer = document.getElementById('jokesContainer');
+    jokesContainer.innerHTML = '<p>Error loading jokes. Please try again.</p>';
+  }
+  
   
